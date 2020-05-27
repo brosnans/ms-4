0bf31751552f63341112b2b90ad5ef0847d6810a
brosnans <s.brosnan3@gmail.com>
2020-05-27 16:43:33 +0000

    Added Stripe API

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_live_rT260yipdaKNB32IvmfQF1sT00lU6pkTaa")
+os.environ.setdefault("STRIPE_SECRET", "sk_live_nAAKAbzu0pZNAEpAQGOZrs3r00hfB60XXo")
