fed768e2eba2cccce3d91b1c9dfcdd03826e5b54
brosnans <s.brosnan3@gmail.com>
2020-05-31 21:09:16 +0000

    Added secret key to env.py

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_live_rT260yipdaKNB32IvmfQF1sT00lU6pkTaa")
 os.environ.setdefault("STRIPE_SECRET", "sk_live_nAAKAbzu0pZNAEpAQGOZrs3r00hfB60XXo")
+os.environ.setdefault("DATABASE_URL", "postgres://ntdaajdoiohgyq:dea8ab8787332600e2e2aa9135f921795bf9b788fe41c60bfd442a972221ad2d@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d713snp5e8hh04")
+os.environ.setdefault("SECRET_KEY", "q*$^kvdkn4p16#)bqun8^(80oi)yq!_3(s(8+cl#_)3lyzxu-n")
