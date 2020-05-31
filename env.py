e0c903d98d2a19c5d48a512d805bdc3ffaa2bd36
brosnans <s.brosnan3@gmail.com>
2020-05-31 23:01:04 +0000

    Connected to AWS S3

--- env.py
+++ env.py
@@ -4,3 +4,5 @@ os.environ.setdefault("STRIPE_PUBLISHABLE", "pk_live_rT260yipdaKNB32IvmfQF1sT00l
 os.environ.setdefault("STRIPE_SECRET", "sk_live_nAAKAbzu0pZNAEpAQGOZrs3r00hfB60XXo")
 os.environ.setdefault("DATABASE_URL", "postgres://ntdaajdoiohgyq:dea8ab8787332600e2e2aa9135f921795bf9b788fe41c60bfd442a972221ad2d@ec2-3-223-21-106.compute-1.amazonaws.com:5432/d713snp5e8hh04")
 os.environ.setdefault("SECRET_KEY", "q*$^kvdkn4p16#)bqun8^(80oi)yq!_3(s(8+cl#_)3lyzxu-n")
+os.environ.setdefault("AWS_ACCESS_KEY_ID", "AKIAQRRJUM3SK326FL4F")
+os.environ.setdefault("AWS_SECRET_ACCESS_KEY_ID", "z05/Zb8JAmlIchEkQSCM9JS9k0WRhN66w+Wm5/Er")
